c75cfa28e5230aa8758cf650a237ef7059a57d12
Jorge Martín <jorgem@element.io>
2026-04-24 14:17:37 +0200

    Increase border

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/TimelineItemCallNotifyView.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/TimelineItemCallNotifyView.kt
@@ -50,7 +50,7 @@ internal fun TimelineItemCallNotifyView(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .border(1.dp, ElementTheme.colors.borderInteractiveSecondary, RoundedCornerShape(8.dp))
+            .border(5.dp, ElementTheme.colors.borderInteractiveSecondary, RoundedCornerShape(8.dp))
             .combinedClickable(
                 enabled = true,
                 onClick = {},
